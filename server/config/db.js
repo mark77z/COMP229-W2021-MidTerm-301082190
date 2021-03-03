56e0b22c68b96803693ad08113565f0e23a68532
Marco Mejia <markmejiar@gmail.com>
2021-03-03 14:53:27 -0500

    Modified DB connection URI to connect to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  // MongoDB Altras deployment ->
+  "URI": "mongodb+srv://markmejiar:qtv25vwN6Dc8nFKE@mongodb.gg1ap.mongodb.net/books229?retryWrites=true&w=majority"
 };
